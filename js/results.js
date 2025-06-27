89efeb6fb85df8a3775a71a382fbb6779673844d
amorancho <alvaromm22@gmail.com>
2025-06-27 13:16:40 +0200

    empezamos modal de detalle de votaciones

--- js/results.js
+++ js/results.js
@@ -1,104 +1,202 @@
 var title = 'Results';
 
+const mockData = {
+  'Junior': {
+    general: [
+    { position: 1, name: 'Amina Hassan', country: 'EG', score: 93.5 },
+    { position: 2, name: 'Lina Torres', country: 'ES', score: 92.0 },
+    { position: 3, name: 'Zara Malik', country: 'IN', score: 91.2 },
+    { position: 4, name: 'Sofia Chen', country: 'CN', score: 90.5 },
+    { position: 5, name: 'Nora Haddad', country: 'FR', score: 89.3 },
+    { position: 6, name: 'Lucía Romero', country: 'AR', score: 88.7 },
+    { position: 7, name: 'Fatima Noor', country: 'PK', score: 87.9 },
+    { position: 8, name: 'Yasmine Ali', country: 'MA', score: 86.5 },
+    { position: 9, name: 'Nadia Karim', country: 'DZ', score: 85.2 },
+    { position: 10, name: 'Camila Díaz', country: 'CL', score: 84.6 }
+    ],
+    styles: {
+    'Oriental': [
+        { position: 1, name: 'Amina Hassan', country: 'EG', score: 94.1 },
+        { position: 2, name: 'Lina Torres', country: 'ES', score: 92.3 },
+        { position: 3, name: 'Zara Malik', country: 'IN', score: 91.9 },
+        { position: 4, name: 'Lucía Romero', country: 'AR', score: 89.5 },
+        { position: 5, name: 'Nadia Karim', country: 'DZ', score: 87.1 }
+    ],
+    'Folklore': [
+        { position: 1, name: 'Fatima Noor', country: 'PK', score: 90.7 },
+        { position: 2, name: 'Sofia Chen', country: 'CN', score: 89.3 },
+        { position: 3, name: 'Nora Haddad', country: 'FR', score: 88.8 },
+        { position: 4, name: 'Camila Díaz', country: 'CL', score: 86.0 }
+    ],
+    'Fusión': [
+        { position: 1, name: 'Yasmine Ali', country: 'MA', score: 92.2 },
+        { position: 2, name: 'Zara Malik', country: 'IN', score: 90.1 },
+        { position: 3, name: 'Lucía Romero', country: 'AR', score: 89.0 }
+    ],
+    'Saidi': [
+        { position: 1, name: 'Sofia Chen', country: 'CN', score: 91.4 },
+        { position: 2, name: 'Amina Hassan', country: 'EG', score: 90.9 },
+        { position: 3, name: 'Yasmine Ali', country: 'MA', score: 89.7 },
+        { position: 4, name: 'Nadia Karim', country: 'DZ', score: 87.6 }
+    ],
+    'Baladi': [
+        { position: 1, name: 'Lina Torres', country: 'ES', score: 93.3 },
+        { position: 2, name: 'Fatima Noor', country: 'PK', score: 91.2 },
+        { position: 3, name: 'Camila Díaz', country: 'CL', score: 89.9 },
+        { position: 4, name: 'Nora Haddad', country: 'FR', score: 88.5 }
+    ]
+    }
+},
+  'Senior': {
+    general: [
+        { position: 1, name: 'Nadia Karim', country: 'MA', score: 95.4 },
+        { position: 2, name: 'Camila Díaz', country: 'AR', score: 94.0 },
+        { position: 3, name: 'Fatima Noor', country: 'PK', score: 92.3 },
+        { position: 4, name: 'Leila Ahmed', country: 'EG', score: 91.5 },
+        { position: 5, name: 'Soraya Bensalem', country: 'DZ', score: 90.2 },
+        { position: 6, name: 'Mariana Silva', country: 'BR', score: 89.8 },
+        { position: 7, name: 'Ines Bouazizi', country: 'TN', score: 88.6 },
+        { position: 8, name: 'Samira Al-Masri', country: 'SA', score: 87.9 },
+        { position: 9, name: 'Valeria Russo', country: 'IT', score: 86.7 },
+        { position: 10, name: 'Aya Yamamoto', country: 'JP', score: 85.5 }
+    ],
+    styles: {
+        'Oriental': [
+        { position: 1, name: 'Nadia Karim', country: 'MA', score: 96.2 },
+        { position: 2, name: 'Leila Ahmed', country: 'EG', score: 94.5 },
+        { position: 3, name: 'Fatima Noor', country: 'PK', score: 93.1 },
+        { position: 4, name: 'Samira Al-Masri', country: 'SA', score: 90.8 },
+        { position: 5, name: 'Valeria Russo', country: 'IT', score: 89.0 }
+        ],
+        'Folklore': [
+        { position: 1, name: 'Soraya Bensalem', country: 'DZ', score: 93.7 },
+        { position: 2, name: 'Mariana Silva', country: 'BR', score: 92.6 },
+        { position: 3, name: 'Camila Díaz', country: 'AR', score: 91.4 },
+        { position: 4, name: 'Ines Bouazizi', country: 'TN', score: 90.2 }
+        ],
+        'Fusión': [
+        { position: 1, name: 'Aya Yamamoto', country: 'JP', score: 92.1 },
+        { position: 2, name: 'Fatima Noor', country: 'PK', score: 91.3 },
+        { position: 3, name: 'Samira Al-Masri', country: 'SA', score: 90.5 }
+        ],
+        'Saidi': [
+        { position: 1, name: 'Leila Ahmed', country: 'EG', score: 94.7 },
+        { position: 2, name: 'Soraya Bensalem', country: 'DZ', score: 93.3 },
+        { position: 3, name: 'Nadia Karim', country: 'MA', score: 92.6 },
+        { position: 4, name: 'Camila Díaz', country: 'AR', score: 91.1 }
+        ],
+        'Baladi': [
+        { position: 1, name: 'Ines Bouazizi', country: 'TN', score: 93.0 },
+        { position: 2, name: 'Mariana Silva', country: 'BR', score: 91.8 },
+        { position: 3, name: 'Valeria Russo', country: 'IT', score: 90.7 },
+        { position: 4, name: 'Aya Yamamoto', country: 'JP', score: 89.4 }
+        ]
+    }
+    }
+
+};
+
+const mockvotingDetails = {
+  'Junior': [
+    {
+      judge: 'Amina Hassan', 
+      totalScore: 9.5, 
+      criteria: [
+        { name: 'Technique', score: 9.0 },
+        { name: 'Interpretation', score: 9.5 },
+        { name: 'Musicality', score: 10.0 },
+        { name: 'Expression', score: 9.0 }
+      ]
+    },
+    {
+      judge: 'Lina Torres', 
+      totalScore: 9.0, 
+      criteria: [
+        { name: 'Technique', score: 8.5 },
+        { name: 'Interpretation', score: 9.0 },
+        { name: 'Musicality', score: 9.5 },
+        { name: 'Expression', score: 9.0 }
+      ]
+    },
+    {
+        judge: 'Zara Malik', 
+        totalScore: 9.2, 
+        criteria: [
+        { name: 'Technique', score: 9.0 },
+        { name: 'Interpretation', score: 9.0 },
+        { name: 'Musicality', score: 9.5 },
+        { name: 'Expression', score: 9.0 }
+      ]
+    }
+  ],
+  'Senior': [
+    {
+      judge: 'Fatima Noor', 
+      totalScore: 9.3, 
+      criteria: [
+        { name: 'Technique', score: 9.0 },
+        { name: 'Interpretation', score: 9.5 },
+        { name: 'Musicality', score: 9.0 },
+        { name: 'Expression', score: 9.0 }
+      ]
+    },
+    {
+      judge: 'Sofia Chen', 
+      totalScore: 8.8, 
+      criteria: [
+        { name: 'Technique', score: 8.5 },
+        { name: 'Interpretation', score: 9.0 },
+        { name: 'Musicality', score: 8.5 },
+        { name: 'Expression', score: 8.5 }
+      ]
+    }
+  ],
+};
+
 document.addEventListener('DOMContentLoaded', () => {
     const categorySelect = document.getElementById('categorySelect');
     const resultsContainer = document.getElementById('resultsContainer');
-  
-    const mockData = {
-      'Junior': {
-        general: [
-        { position: 1, name: 'Amina Hassan', country: 'EG', score: 93.5 },
-        { position: 2, name: 'Lina Torres', country: 'ES', score: 92.0 },
-        { position: 3, name: 'Zara Malik', country: 'IN', score: 91.2 },
-        { position: 4, name: 'Sofia Chen', country: 'CN', score: 90.5 },
-        { position: 5, name: 'Nora Haddad', country: 'FR', score: 89.3 },
-        { position: 6, name: 'Lucía Romero', country: 'AR', score: 88.7 },
-        { position: 7, name: 'Fatima Noor', country: 'PK', score: 87.9 },
-        { position: 8, name: 'Yasmine Ali', country: 'MA', score: 86.5 },
-        { position: 9, name: 'Nadia Karim', country: 'DZ', score: 85.2 },
-        { position: 10, name: 'Camila Díaz', country: 'CL', score: 84.6 }
-        ],
-        styles: {
-        'Oriental': [
-            { position: 1, name: 'Amina Hassan', country: 'EG', score: 94.1 },
-            { position: 2, name: 'Lina Torres', country: 'ES', score: 92.3 },
-            { position: 3, name: 'Zara Malik', country: 'IN', score: 91.9 },
-            { position: 4, name: 'Lucía Romero', country: 'AR', score: 89.5 },
-            { position: 5, name: 'Nadia Karim', country: 'DZ', score: 87.1 }
-        ],
-        'Folklore': [
-            { position: 1, name: 'Fatima Noor', country: 'PK', score: 90.7 },
-            { position: 2, name: 'Sofia Chen', country: 'CN', score: 89.3 },
-            { position: 3, name: 'Nora Haddad', country: 'FR', score: 88.8 },
-            { position: 4, name: 'Camila Díaz', country: 'CL', score: 86.0 }
-        ],
-        'Fusión': [
-            { position: 1, name: 'Yasmine Ali', country: 'MA', score: 92.2 },
-            { position: 2, name: 'Zara Malik', country: 'IN', score: 90.1 },
-            { position: 3, name: 'Lucía Romero', country: 'AR', score: 89.0 }
-        ],
-        'Saidi': [
-            { position: 1, name: 'Sofia Chen', country: 'CN', score: 91.4 },
-            { position: 2, name: 'Amina Hassan', country: 'EG', score: 90.9 },
-            { position: 3, name: 'Yasmine Ali', country: 'MA', score: 89.7 },
-            { position: 4, name: 'Nadia Karim', country: 'DZ', score: 87.6 }
-        ],
-        'Baladi': [
-            { position: 1, name: 'Lina Torres', country: 'ES', score: 93.3 },
-            { position: 2, name: 'Fatima Noor', country: 'PK', score: 91.2 },
-            { position: 3, name: 'Camila Díaz', country: 'CL', score: 89.9 },
-            { position: 4, name: 'Nora Haddad', country: 'FR', score: 88.5 }
-        ]
-        }
-    },
-      'Senior': {
-        general: [
-            { position: 1, name: 'Nadia Karim', country: 'MA', score: 95.4 },
-            { position: 2, name: 'Camila Díaz', country: 'AR', score: 94.0 },
-            { position: 3, name: 'Fatima Noor', country: 'PK', score: 92.3 },
-            { position: 4, name: 'Leila Ahmed', country: 'EG', score: 91.5 },
-            { position: 5, name: 'Soraya Bensalem', country: 'DZ', score: 90.2 },
-            { position: 6, name: 'Mariana Silva', country: 'BR', score: 89.8 },
-            { position: 7, name: 'Ines Bouazizi', country: 'TN', score: 88.6 },
-            { position: 8, name: 'Samira Al-Masri', country: 'SA', score: 87.9 },
-            { position: 9, name: 'Valeria Russo', country: 'IT', score: 86.7 },
-            { position: 10, name: 'Aya Yamamoto', country: 'JP', score: 85.5 }
-        ],
-        styles: {
-            'Oriental': [
-            { position: 1, name: 'Nadia Karim', country: 'MA', score: 96.2 },
-            { position: 2, name: 'Leila Ahmed', country: 'EG', score: 94.5 },
-            { position: 3, name: 'Fatima Noor', country: 'PK', score: 93.1 },
-            { position: 4, name: 'Samira Al-Masri', country: 'SA', score: 90.8 },
-            { position: 5, name: 'Valeria Russo', country: 'IT', score: 89.0 }
-            ],
-            'Folklore': [
-            { position: 1, name: 'Soraya Bensalem', country: 'DZ', score: 93.7 },
-            { position: 2, name: 'Mariana Silva', country: 'BR', score: 92.6 },
-            { position: 3, name: 'Camila Díaz', country: 'AR', score: 91.4 },
-            { position: 4, name: 'Ines Bouazizi', country: 'TN', score: 90.2 }
-            ],
-            'Fusión': [
-            { position: 1, name: 'Aya Yamamoto', country: 'JP', score: 92.1 },
-            { position: 2, name: 'Fatima Noor', country: 'PK', score: 91.3 },
-            { position: 3, name: 'Samira Al-Masri', country: 'SA', score: 90.5 }
-            ],
-            'Saidi': [
-            { position: 1, name: 'Leila Ahmed', country: 'EG', score: 94.7 },
-            { position: 2, name: 'Soraya Bensalem', country: 'DZ', score: 93.3 },
-            { position: 3, name: 'Nadia Karim', country: 'MA', score: 92.6 },
-            { position: 4, name: 'Camila Díaz', country: 'AR', score: 91.1 }
-            ],
-            'Baladi': [
-            { position: 1, name: 'Ines Bouazizi', country: 'TN', score: 93.0 },
-            { position: 2, name: 'Mariana Silva', country: 'BR', score: 91.8 },
-            { position: 3, name: 'Valeria Russo', country: 'IT', score: 90.7 },
-            { position: 4, name: 'Aya Yamamoto', country: 'JP', score: 89.4 }
-            ]
-        }
-        }
 
-    };
+    const editModal = new bootstrap.Modal(document.getElementById('votingDetailsModal'));
+
+    document.addEventListener('click', (event) => {
+      const spanDancer = event.target.closest('.dancer-result');
+
+      if (spanDancer) {
+        const dancerName = spanDancer.textContent.trim();
+
+        const detailsContainer = document.getElementById('votingDetailsContainer');
+
+        detailsContainer.innerHTML = ''; // Limpiar contenido previo
+
+        const category = categorySelect.value;
+        const votingDetails = mockvotingDetails[category] || [];
+
+        votingDetails.forEach(detail => {
+
+          const judgeDiv = document.createElement('div');
+          judgeDiv.className = 'mb-3';
+          judgeDiv.innerHTML = `
+            <h5 class="text-primary">${detail.judge}</h5>
+            <p class="text-secondary">Total Score: <strong>${detail.totalScore.toFixed(1)}</strong></p>
+            <ul class="list-group">
+              ${detail.criteria.map(c => `
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                  ${c.name}
+                  <span class="badge bg-light text-dark rounded-pill">${c.score.toFixed(1)}</span>
+                </li>
+              `).join('')}
+            </ul>
+          `;
+          detailsContainer.appendChild(judgeDiv);
+        });
+
+        document.querySelector('#votingDetailsModal .modal-title span').textContent = dancerName;
+        editModal.show();
+      }
+    });
+  
+    
   
     // Rellenar dropdown
     Object.keys(mockData).forEach(category => {
@@ -165,7 +263,7 @@ document.addEventListener('DOMContentLoaded', () => {
           <div class="card-body">
             <div class="d-flex justify-content-center align-items-center gap-2 mb-3">
               <img src="https://flagsapi.com/${list[0].country}/shiny/24.png" class="img-fluid" alt="${list[0].country}" width="24" height="24">
-              <h3 class="mb-0">${list[0].name}</h3>
+              <h3 class="mb-0 dancer-result">${list[0].name}</h3>
             </div>
             <p class="card-text fs-5">Score: ${list[0].score}</p>
           </div>
@@ -185,7 +283,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <div class="card-body">
               <div class="d-flex justify-content-center align-items-center gap-2 mb-3">
                 <img src="https://flagsapi.com/${list[1].country}/shiny/24.png" class="img-fluid" alt="${list[1].country}" width="24" height="24">
-                <h3 class="mb-0">${list[1].name}</h3>
+                <h3 class="mb-0 dancer-result">${list[1].name}</h3>
               </div>
               <p class="card-text fs-5">Score: ${list[1].score}</p>
             </div>
@@ -199,7 +297,7 @@ document.addEventListener('DOMContentLoaded', () => {
             <div class="card-body">
               <div class="d-flex justify-content-center align-items-center gap-2 mb-3">
                 <img src="https://flagsapi.com/${list[2].country}/shiny/24.png" class="img-fluid" alt="${list[2].country}" width="24" height="24">
-                <h3 class="mb-0">${list[2].name}</h3>
+                <h3 class="mb-0 dancer-result">${list[2].name}</h3>
               </div>
               <p class="card-text fs-5">Score: ${list[2].score}</p>
             </div>
@@ -219,7 +317,7 @@ document.addEventListener('DOMContentLoaded', () => {
         li.innerHTML = `
           <span class="me-2">${item.position}</span>
           <img src="https://flagsapi.com/${item.country}/shiny/24.png" class="me-2" alt="${item.country}">
-          <span class="me-auto">${item.name}</span>
+          <span class="me-auto dancer-result">${item.name}</span>
           <span class="badge bg-light text-dark rounded-pill">${item.score.toFixed(1)}</span>
         `;
     
@@ -266,7 +364,7 @@ document.addEventListener('DOMContentLoaded', () => {
           li.innerHTML = `
             <span class="me-2">${item.position}</span>
             <img src="https://flagsapi.com/${item.country}/shiny/24.png" class="me-2" alt="${item.country}">
-            <span class="me-auto">${item.name} ${medal}</span>
+            <span class="me-auto dancer-result">${item.name} ${medal}</span>
             <span class="badge bg-light text-dark rounded-pill">${item.score.toFixed(1)}</span>
           `;
       
